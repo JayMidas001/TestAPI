b9f773631efbda54bfa2589b8725848b27eeef05
Cisca_ella <nwokoella@gmail.com>
2024-09-02 07:01:00 +0100

    New Update 1.2

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -2,18 +2,24 @@ const validator = require('@hapi/joi')
 
 const schemas = {
     
-    businessName:validator.string().min(3).required().pattern(new RegExp(/^[^\s].+[^\s]$/)).messages({
+    businessName:validator.string().min(6).required().pattern(new RegExp(/^[^\s].+[^\s]$/)).messages({
         "any.required": "Business name is required.",
-        "string.empty": "Business name cannot be an empty string.",
-        "string.min": "Business name must be at least 3 characters long.",
-        "string.pattern.base": "Business name cannot start or end with a whitespace."
+        "string.empty": "Business name cannot be empty.",
+        "string.min": "Business name must be at least 6 characters long.",
+        "string.pattern.base": "Business name cannot start or end with a whitespace.",
+        "string.base": "Business name cannot be empty."
       }),
-    fullName:validator.string().min(3).required().pattern(new RegExp(/^[^\s].+[^\s]$/)).messages({
-        "any.required": "Fullname is required.",
-        "string.empty": "Fullname cannot be an empty string.",
-        "string.min": "Full name must be at least 3 characters long.",
-        "string.pattern.base": "Full name cannot start or end with a whitespace."
-      }),
+    fullName:validator.string()
+    .min(6)
+    .pattern(new RegExp("^[A-Za-z]+\\s+[A-Za-z]+$"))
+    .required()
+    .messages({
+      "any.required": "Full name is required.",
+      "string.empty": "Full name cannot be empty.",
+      "string.min": "Full name must be at least 6 characters long.",
+      "string.pattern.base": "Please provide both first and last names separated by a space.",
+      "string.base": "Full name cannot be empty."
+    }),
     email:validator.string().email().required().messages({
         "any.required": "Email is required.",
         "string.email": "Invalid email format.",
@@ -29,12 +35,13 @@ const schemas = {
       }),
       
     password:validator.string()
-    .pattern(new RegExp("^(?=.*[!@#$%^&*])(?=.*[A-Z]).{8,}$"))
+    .pattern(new RegExp("^(?=.*[!@#$%^&*.])(?=.*[A-Z]).{8,}$"))
     .required()
     .messages({
       "any.required": "Password is required.",
+      "string.base": "Password must contain at least 8 characters, one capital letter, and one special character (!@#$%^&*.).",
       "string.pattern.base":
-        "Password must contain at least 8 characters, one capital letter, and one special character (!@#$%^&*).",}),
+        "Password must contain at least 8 characters, one capital letter, and one special character (!@#$%^&*.).",}),
     address:validator.string().required().regex(/^[a-zA-Z0-9-,\. ]+$/).messages({
         'string.pattern.base': 'Address can contain only alphabetic characters, numbers, spaces, or punctuations.',
         'any.required': 'Address is required.',
